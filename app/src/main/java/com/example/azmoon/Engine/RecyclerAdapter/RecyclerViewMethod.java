5f30c6bb80b4d7e586615ce582879d693493b394
hani6104 <hanieh.1319@gmail.com>
2021-02-07 18:05:10 +0330

    change2

--- app/src/main/java/com/example/azmoon/Engine/RecyclerAdapter/RecyclerViewMethod.java
+++
@@ -1,7 +0,0 @@
-package com.example.azmoon.Engine.RecyclerAdapter;
-
-import android.view.View;
-
-public interface RecyclerViewMethod {
-    public void onItem(RecyclerViewAdapter.ViewHolder holder, int position, View itemView);
-}
--- app/src/main/java/com/example/azmoon/OnResult.java
+++
@@ -1,6 +0,0 @@
-package com.example.azmoon;
-
-public interface OnResult {
-    public void success(Object...objects);
-//    public void onFailed(Exception e);
-}
